c5c7f562ceb557fe656af1d02461347de9318a5e
Anujith vk <anolianu123@gmail.com>
2024-10-31 15:38:33 +0530

    THIRD

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ const cors=require('cors')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
-app.use(cors())
+app.use(cors(
+    {
+        origin:'https://blog-website-1-4svg.onrender.com',
+        allowedHeaders:['GET','PUT','POST','DELETE']
+    }
+))
 app.use('/',Route)
 
 app.listen(port,()=>{
